0835d80599af01a690a2e7b1ab7800f2737c2283
Scott van den Broek <scottzico2004@gmail.com>
2022-09-27 12:51:22 +0200

    logout toegevoegd aan navigation en onnodige heaing verwijderd

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,45 +4,37 @@
         <div class="flex justify-center h-16">
             <div class="flex">
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex px-20">
+                <div class="hidden space-x-8 sm:-my-px sm:flex px-20">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('My Activities') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex px-20">
+                <div class="hidden space-x-8 sm:-my-px sm:flex px-20">
                     <x-nav-link :href="route('manage-activities')" :active="request()->routeIs('manage-activities')">
-                        {{ __('Manage-Activities') }}
+                        {{ __('Manage Activities') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex px-20">
+                <div class="hidden space-x-8 sm:-my-px sm:flex px-20">
                     <x-nav-link :href="route('new-activity')" :active="request()->routeIs('new-activity')">
-                        {{ __('New-activity') }}
+                        {{ __('New Activity') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex px-20">
+                <div class="hidden space-x-8 sm:-my-px sm:flex px-20">
                     <x-nav-link :href="route('manage-account')" :active="request()->routeIs('manage-account')">
-                        {{ __('Manage-Accout') }}
+                        {{ __('Manage Accout') }}
                     </x-nav-link>
                 </div>
-            </div>
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+                <form method="POST" action="{{ route('logout') }}" class="hidden space-x-8 sm:-my-px sm:flex px-20">
+                    @csrf
+                    <x-nav-link :href="route('logout')"
+                                     onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-nav-link>
+                </form>
             </div>
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-    </div>
+
 </nav>
--- resources/views/manage-account.blade.php
+++ resources/views/manage-account.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <h1>manage acount</h1>
     </div>
