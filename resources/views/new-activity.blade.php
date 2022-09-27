6f3734bbd7b5341a71bf9cc72299b60998e8cdc3
Scott van den Broek <scottzico2004@gmail.com>
2022-09-27 15:13:11 +0200

    css van new activity form gemaakt

--- resources/views/new-activity.blade.php
+++ resources/views/new-activity.blade.php
@@ -1,11 +1,22 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <h1>New Activity</h1>
+    <div class="p-12 flex flex-col items-center font-bold text-blue">
+        <h1 class="text-6xl p-5">New Activity</h1>
+        <form action="">
+            <div>
+                <h3>Name Activity</h3>
+                <input class="border-blue border-2" type="text">
+            </div>
+            <div>
+                <h3>Day</h3>
+                <input class="border-blue border-2" type="date">
+            </div>
+            <div>
+                <h3>Name Activity</h3>
+                <textarea class="border-blue border-2" cols="30" rows="10"></textarea>
+            </div>
+            <div class="flex justify-end">
+                <button type="submit" class="inline-flex items-center px-4 py-2 bg-orange rounded-md font-bold text-base text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none disabled:opacity-25 transition ease-in-out duration-150">Create</button>
+            </div>
+        </form>
     </div>
 </x-app-layout>
